3e4b2c8b9c4025ef8f0e0bf3070f2a3f152bfc70
Jeshua <148483968+Jeshua3545926@users.noreply.github.com>
2024-10-24 12:35:52 -0600

    nuevos cambios

--- Untitled-1.php
+++ Untitled-1.php
@@ -10,22 +10,36 @@ function gola($nombre){
     
     $input = trim(fgets(STDIN));
     echo "hola, $input" .PHP_EOL;
-if ($input == "Jeshua" || $input == " jeshua"){
-                $estructura = [
-                "Programaador: Senior",
-                "Aprendizaje: 100%",
-                "Programaador: Junior",
-                "Aprendizaje: 20%",
-                "Programaador: Semi Senior",
-                "Aprendizaje: 40%",
-                "Programaador: SQL (Base De Datos)",
-                "Aprendizaje: 55%",
-                "Programaador: Desarrollador Web",
-                "Aprendizaje: 50%",
-                "Programaador: Desarrollador De IA",
-                "Aprendizaje: 100%",
-    ];
-   foreach($estructura as $linea){
-    echo $linea.PHP_EOL;
-   }
+    if ($input == "Jeshua" || $input == " jeshua"){
+        echo "hola, $input" .PHP_EOL;
+
+        $colores = [
+            "\033[31m",  //Rojo
+            "\033[31m",  
+            "\033[31m", 
+            "\033[31m",  
+            "\033[31m",
+            "\033[31m" ,
+        ];
+
+        $estructura = [
+        "Programaador: Senior",
+        "Aprendizaje: 100%",
+        "Programaador: Junior",
+        "Aprendizaje: 20%",
+        "Programaador: Semi Senior",
+        "Aprendizaje: 40%",
+        "Programaador: SQL (Base De Datos)",
+        "Aprendizaje: 55%",
+        "Programaador: Desarrollador Web",
+        "Aprendizaje: 50%",
+        "Programaador: Desarrollador De IA",
+        "Aprendizaje: 100%",
+        ];
+
+foreach($estructura as $index => $linea){
+$color = $colores[$index % count($colores)];
+echo  $color . $linea . "\033[0m" . PHP_EOL;
+}
+echo $estructura[0];
 }
